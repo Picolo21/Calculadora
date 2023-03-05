a07187e5232c22d2c4cfc6942124e44199b7ce4b
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-03-05 16:46:14 -0300

    Calculadora com a função Menu

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,9 +1,59 @@
-﻿internal class Program
+﻿using System.Diagnostics;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         double number1;
         double number2;
+        int option = 0;
+
+        void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("[1] - Somar");
+            Console.WriteLine("[2] - Subtrair");
+            Console.WriteLine("[3] - Multiplicar");
+            Console.WriteLine("[4] - Dividir");
+            Console.WriteLine("[5] - Sair");
+            Console.Write("Escolha uma operação matemática: ");
+            option = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("A soma é igual a " + (number1 + number2).ToString("F2"));
+                    Console.ReadKey();    
+                    break;
+                case 2:
+                    Console.WriteLine("A subtração é igual a " + (number1 - number2).ToString("F2"));
+                    Console.ReadKey();
+                    break;
+                case 3:
+                    Console.WriteLine("A multiplicação é igual a " + (number1 * number2).ToString("F2"));
+                    Console.ReadKey();
+                    break;
+                case 4:
+                    if (number2 == 0)
+                    {
+                        Console.WriteLine("Não é possível realizar a divisão por ZERO!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("A divisão é igual a " + (number1 / number2).ToString("F2"));
+                    }
+                    Console.ReadKey();
+                    break;
+                case 5:
+                    Console.WriteLine("Ok, até mais!");
+                    break;
+                default:
+                    Console.WriteLine("Valor informado inválido. Por favor, aperte a tecla ENTER e digite um valor válido");
+                    Console.ReadKey();
+                    break;
+            }
+        }
 
         Console.Write("Digite o 1° número: ");
         number1 = double.Parse(Console.ReadLine());
@@ -11,16 +61,9 @@
         number2 = double.Parse(Console.ReadLine());
         Console.WriteLine();
 
-        Console.WriteLine("A soma é igual a " + (number1 + number2).ToString("F2"));
-        Console.WriteLine("A subtração é igual a " + (number1 - number2).ToString("F2"));
-        Console.WriteLine("A multiplicação é igual a " + (number1 * number2).ToString("F2"));
-        if (number2 == 0)
+        while (option != 5)
         {
-            Console.WriteLine("Não é possível realizar a divisão por ZERO!");
-        }
-        else
-        {
-            Console.WriteLine("A divisão é igual a " + (number1 / number2).ToString("F2"));
+            Menu();
         }
     }
 }
